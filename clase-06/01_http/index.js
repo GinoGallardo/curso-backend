2db542185033a9f1693a0c6f4d168d6aeb8f559f
GinoGallardo <ginosgt@gmail.com>
2023-09-11 23:00:31 -0300

    actualizando repo

--- clase-06/01_http/index.js
+++ clase-06/01_http/index.js
@@ -1,13 +1,15 @@
-const { log } = require('console');
+// const { log } = require('console');
 const http = require('http');
 
 // creamos el server
 const server = http.createServer((request, response) => {
   response.end('Mi primer hola mundo desde el server con modulo nativo HTTP!!!, actualizando con NODEMON')
+  // el .end es para enviar la respuesta del
+  // servidor hacia el cliente
 })
 
 
 // confi puerto de escucha
-app.listen(PORT, () => {
-  console.log(`Server run on port: ${PORT}`);
+server.listen(8080, () => {
+  console.log(`Server run on port: 8080`);
 })
